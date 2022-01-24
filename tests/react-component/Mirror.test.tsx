ca6d98aa25f8aaae9baef53effff8924310c4a35
Juunini (쥬니니) <juuni.ni.i@gmail.com>
2022-01-25 08:59:03 +0900

    Fix test

--- tests/react-component/Mirror.test.tsx
+++ tests/react-component/Mirror.test.tsx
@@ -1,54 +1,40 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import Mirror from '../../src/react-component/Mirror';
+import { MirrorEntity, PlaneMirror } from '../../src/react-component/Mirror';
 
-describe('Mirror', () => {
+describe('PlaneMirror', () => {
   context ('When gives no one', () => {
-    it('Should render has default position and empty mirror properties a-plane tag', () => {
-      const { container } = render(<Mirror />);
+    it('Should render empty mirror properties a-plane tag', () => {
+      const { container } = render(<PlaneMirror />);
 
-      expect(container.innerHTML).toBe('<a-plane position="0 0 20" mirror=""></a-plane>');
-    });
-  });
-
-  context ('When gives position', () => {
-    it('Should render has given position and empty mirror properties a-plane tag', () => {
-      const { container } = render(<Mirror position={{ x: 0, y: 0, z: 0 }} />);
-
-      expect(container.innerHTML).toBe('<a-plane position="0 0 0" mirror=""></a-plane>');
-    });
-  });
-
-  context ('When gives width', () => {
-    it('Should render has default position and width mirror properties a-plane tag', () => {
-      const { container } = render(<Mirror width={1} />);
-
-      expect(container.innerHTML).toBe('<a-plane position="0 0 20" mirror="width:1;"></a-plane>');
-    });
-  });
-
-  context ('When gives height', () => {
-    it('Should render has default position and height mirror properties a-plane tag', () => {
-      const { container } = render(<Mirror height={1} />);
-
-      expect(container.innerHTML).toBe('<a-plane position="0 0 20" mirror="height:1;"></a-plane>');
-    });
-  });
-
-  context ('When gives width and height', () => {
-    it('Should render has default position and width, height mirror properties a-plane tag', () => {
-      const { container } = render(<Mirror width={1} height={1} />);
-
-      expect(container.innerHTML).toBe('<a-plane position="0 0 20" mirror="width:1;height:1;"></a-plane>');
+      expect(container.innerHTML).toBe('<a-plane mirror="true"></a-plane>');
     });
   });
 
   context('When gives custom props', () => {
     it('Should render has custom props a-plane tag', () => {
-      const { container } = render(<Mirror custom="value1:true;value2:#sample;" />);
+      const { container } = render(<PlaneMirror custom="value1:true;value2:#sample;" />);
 
-      expect(container.innerHTML).toBe('<a-plane position="0 0 20" mirror="" custom="value1:true;value2:#sample;"></a-plane>');
+      expect(container.innerHTML).toBe('<a-plane mirror="true" custom="value1:true;value2:#sample;"></a-plane>');
+    });
+  });
+});
+
+describe('MirrorEntity', () => {
+  context ('When gives no one', () => {
+    it('Should render empty mirror properties a-entity tag', () => {
+      const { container } = render(<MirrorEntity geometry={{ primitive: 'box' }} />);
+
+      expect(container.innerHTML).toBe('<a-entity geometry="primitive:box;" mirror="true"></a-entity>');
+    });
+  });
+
+  context('When gives custom props', () => {
+    it('Should render has custom props a-entity tag', () => {
+      const { container } = render(<MirrorEntity custom="value1:true;value2:#sample;" />);
+
+      expect(container.innerHTML).toBe('<a-entity mirror="true" custom="value1:true;value2:#sample;"></a-entity>');
     });
   });
 });
